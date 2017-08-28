e61e29109a751fb60dbf9718b3e0b5ae5df5de44
Robin Davies <robbiedaves@hotmail.com>
2017-08-28 18:54:06 +0100

    adding provideDelegate function

--- src/com/robbiedaves/kotlintrial/delegates/delegateproperties/RobDelegateC.kt
+++ src/com/robbiedaves/kotlintrial/delegates/delegateproperties/RobDelegateC.kt
@@ -1,5 +1,6 @@
 package com.robbiedaves.kotlintrial.delegates.delegateproperties
 
+import javafx.beans.property.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class RobDelegateC {
@@ -11,4 +12,12 @@ class RobDelegateC {
         println("$value has been assigned to '${property.name} in $thisRef.'")
     }
 
+    //Robxx - this is wrong - we need to understand this better!
+//    operator fun provideDelegate(thisRef :RobClass?, property: KProperty<*>) : String {
+//        return checkRobProperty(thisRef)
+//    }
+
+    private fun checkRobProperty(thisRef: RobClass?) : String {
+        return "ROBXX******* RobPropertyChecked: " + thisRef?.getRobClassText()
+    }
 }
